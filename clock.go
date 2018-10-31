cab45e1d751f49d092683cef2996423a264d2d33
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-10-31 16:35:36 +0100

    Remove unused clock func implementations

--- clock.go
+++ clock.go
@@ -21,23 +21,3 @@ type Sleeper interface {
 	// Sleep sleeps for a certain amount of time.
 	Sleep(d time.Duration)
 }
-
-type clockFunc struct {
-	nowFn   func() time.Time
-	sleepFn func(time.Duration)
-}
-
-// ClockFunc returns a new struct from the passed functions.
-func ClockFunc(nowFn func() time.Time, sleepFn func(time.Duration)) Clock {
-	return &clockFunc{nowFn, sleepFn}
-}
-
-// Now tells the current time.
-func (c *clockFunc) Now() time.Time {
-	return c.nowFn()
-}
-
-// Sleep sleeps for a certain amount of time.
-func (c *clockFunc) Sleep(d time.Duration) {
-	c.sleepFn(d)
-}
